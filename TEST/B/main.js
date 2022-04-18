efc76a8e09418a54e01093f8aea66237c203eeba
Quy <>
2022-04-18 22:36:57 +0700

    update

--- TEST/B/main.js
+++ TEST/B/main.js
@@ -1,82 +1,7 @@
 import { itemAnswer } from "./itemAnswer.js";
 
-let handleFunction = (arr) => {
-  class QuizApp {
-    container = document.createElement("div");
-    question = document.createElement("div");
-    answer = document.createElement("div");
-    point = document.createElement("div");
-    itemAnswerApp;
-  
-    arrShowed = [];
-  
-    diem = 0;
-  
-    constructor() {
-      this.container.appendChild(this.point);
-      this.point.innerHTML = "0";
-      this.point.classList.add("point");
-      this.container.classList.add("container");
-      this.container.appendChild(this.question);
-      this.container.appendChild(this.answer);
-      this.answer.classList.add("answer");
-      this.handleShow();
-    }
-  
-    handleShow = () => {
-      let num = Math.floor(Math.random() * arr.length);
-      if (this.arrShowed.includes(num) == false) {
-        this.answer.innerHTML = "";
-        this.question.innerHTML = "";
-        this.arrShowed.push(num);
-        this.question.innerHTML = arr[num].question;
-        let numCorrect = Math.floor(Math.random() * 4);
-        let arrIncorrect = [];
-        let n = 0;
-  
-        for (let i = 0; i < 4; i++) {
-          if (i == numCorrect) {
-            this.itemAnswerApp = new itemAnswer(arr[num].correct_answer);
-            this.answer.appendChild(this.itemAnswerApp.container);
-            this.itemAnswerApp.handleOnClick(() => {
-              this.handleOnClickTrue();
-            });
-          } else {
-            while (true) {
-              let numIncorrect = Math.floor(Math.random() * 3);
-              if (arrIncorrect.includes(numIncorrect) == false) {
-                arrIncorrect.push(numIncorrect);
-                this.itemAnswerApp = new itemAnswer(
-                  arr[num].incorrect_answers[numIncorrect]
-                );
-                this.itemAnswerApp.handleOnClick(() => {
-                  this.handleOnClickFalse();
-                });
-                this.answer.appendChild(this.itemAnswerApp.container);
-                n = n + 1;
-              }
-              if (n == 3) break;
-            }
-          }
-        }
-      } else this.handleShow();
-    };
-  
-    handleOnClickTrue = () => {
-        alert("true");
-        this.diem = this.diem + 10;
-        this.point.innerHTML = this.diem;
-        this.handleShow();
-      
-    };
-  
-    handleOnClickFalse = () => {
-      alert("false");
-      this.handleShow();
-    };
-  }
-  
-  let app = new QuizApp();
+let handleFunction = (data) => {
+  let app = new QuizApp(data);
   document.querySelector("body").appendChild(app.container);
 };
 
@@ -86,7 +11,81 @@ fetch(
   .then((response) => response.json())
   .then((data) => handleFunction(data));
 
+class QuizApp {
+  container = document.createElement("div");
+  question = document.createElement("div");
+  answer = document.createElement("div");
+  point = document.createElement("div");
+  itemAnswerApp;
 
+  arrShowed = [];
+
+  diem = 0;
+  arr = [];
+
+  constructor(data) {
+    this.arr = data;
+    this.container.appendChild(this.point);
+    this.point.innerHTML = "0";
+    this.point.classList.add("point");
+    this.container.classList.add("container");
+    this.container.appendChild(this.question);
+    this.container.appendChild(this.answer);
+    this.answer.classList.add("answer");
+    this.handleShow();
+  }
+
+  handleShow = () => {
+    let num = Math.floor(Math.random() * this.arr.length);
+    if (this.arrShowed.includes(num) == false) {
+      this.answer.innerHTML = "";
+      this.question.innerHTML = "";
+      this.arrShowed.push(num);
+      this.question.innerHTML = this.arr[num].question;
+      let numCorrect = Math.floor(Math.random() * 4);
+      let arrIncorrect = [];
+      let n = 0;
+
+      for (let i = 0; i < 4; i++) {
+        if (i == numCorrect) {
+          this.itemAnswerApp = new itemAnswer(this.arr[num].correct_answer);
+          this.answer.appendChild(this.itemAnswerApp.container);
+          this.itemAnswerApp.handleOnClick(() => {
+            this.handleOnClickTrue();
+          });
+        } else {
+          while (true) {
+            let numIncorrect = Math.floor(Math.random() * 3);
+            if (arrIncorrect.includes(numIncorrect) == false) {
+              arrIncorrect.push(numIncorrect);
+              this.itemAnswerApp = new itemAnswer(
+                this.arr[num].incorrect_answers[numIncorrect]
+              );
+              this.itemAnswerApp.handleOnClick(() => {
+                this.handleOnClickFalse();
+              });
+              this.answer.appendChild(this.itemAnswerApp.container);
+              n = n + 1;
+            }
+            if (n == 3) break;
+          }
+        }
+      }
+    } else this.handleShow();
+  };
+
+  handleOnClickTrue = () => {
+    alert("true");
+    this.diem = this.diem + 10;
+    this.point.innerHTML = this.diem;
+    this.handleShow();
+  };
+
+  handleOnClickFalse = () => {
+    alert("false");
+    this.handleShow();
+  };
+}
 
 // let arr = [
 //   {
@@ -204,7 +203,7 @@ fetch(
 //       this.diem = this.diem + 10;
 //       this.point.innerHTML = this.diem;
 //       this.handleShow();
-    
+
 //   };
 
 //   handleOnClickFalse = () => {
